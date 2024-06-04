afc697541c716faae3e26d7966775a46a0da4712
Erentsen <sangadzhiev1406@mail.ru>
2024-06-04 14:38:20 +0300

    initial commit

---
+++ src/main/java/org/example/kpohw3auth/KpoHw3AuthApplication.java
@@ -0,0 +1,13 @@
+package org.example.kpohw3auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpoHw3AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpoHw3AuthApplication.class, args);
+	}
+
+}
